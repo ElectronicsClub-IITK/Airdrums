4ef7ba21afc28910d04a2aa727f73af68edefcbf
disha318 <direction1819@gmail.com>
2024-05-31 13:19:11 +0530

    Disha_Assignment4.py

--- Week_3/Assignment_4/Disha_Assignment4.py
+++ Week_3/Assignment_4/Disha_Assignment4.py
@@ -1,25 +1,25 @@
 import csv
 
-def reading_acc(acc_path):
-    acc = []
-    with open(acc_path, mode='r') as f:
-        read = csv.reader(f)
-        next(read)
-        for row in read:
-            acc.append([float(row[0]), float(row[1]), float(row[2])])
-    return acc
-
 def update(velocity, displacement, acc, time_elapsed):
+    #using v=u+a*t and s=u*t+0.5*a*t^2
     for i in range(3):   
         displacement[i] += (velocity[i] * time_elapsed) + 0.5 * (acc[i] * time_elapsed **2)
         velocity[i] += acc[i] * time_elapsed
     return velocity, displacement
 
 if __name__ == "__main__":
+    #setting up initial conditions
     displacement = [0.0, 0.0, 0.0]
     velocity = [0.0, 0.0, 0.0]
     time_elapsed = 0.1
-    acc_path = 'imu_data.csv'
-    acc = reading_acc(acc_path)
-    velocity, displacement= update(velocity, displacement, acc, time_elapsed)
-    print(f"Current Velocity: {velocity}, Displacement: {displacement}")
+    file_path = 'imu_data.csv'
+    #opening the csv file in read mode
+    with open(file_path, mode='r') as f:
+        read = csv.reader(f)
+        next(read) #skipping the header
+        #reading the accelerations and printing the updated velocity and displacement vector
+        for row in read:
+            acc = []
+            acc.append([float(row[0]), float(row[1]), float(row[2])])
+            velocity, displacement= update(velocity, displacement, acc, time_elapsed)
+            print(f"Current Velocity: {velocity}, Displacement: {displacement}") 
